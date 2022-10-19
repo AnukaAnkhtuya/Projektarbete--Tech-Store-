8d61136907004276f3a0aec73d9169aac9bd05b8
Amanda <aamandaallinger@outlook.com>
2022-10-19 11:24:47 +0200

    produkterna syns på sidan! (används Live Server)

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var listOfProducts;
+var inCart = [];
 
 /** Get products from the json file and store it in a gobal variable */
 function loadProducts() {
@@ -13,19 +14,79 @@ function loadProducts() {
 }
 
 
+
+
 function initSite() {
     loadProducts();
-    // This would also be a good place to initialize other parts of the UI
+    let itemCart = localStorage.doList;
+    if (itemCart) {
+        inCart = JSON.parse(itemCart);
+    }
+   document.getElementById("itemCounter").innerHTML = inCart.length;
 }
 
 /** Uses the loaded products data to create a visible product list on the website */
 function addProductsToWebpage() {
-    // Check your console to see that the products are stored in the listOfProducts varible.
-    console.log(listOfProducts);
+    let main = document.getElementsByTagName("main")[0];
+    let mainContainer = document.createElement("div");
+    mainContainer.classList = "container";
+    main.appendChild(mainContainer);
 
-    // Add your code here, remember to brake your code in to smaller function blocks
-    // to reduce complexity and increase readability. Each function should have
-    // an explainetory comment like the one for this function, see row 22.
-    
-    // TODO: Remove the console.log and these comments when you've read them.
+    for (let i = 0; i <listOfProducts.length; i++) {
+       let selectedItem = listOfProducts[i];
+
+        let itemCard = document.createElement("div");
+        itemCard.classList = "itemCardDiv";
+        let itemCardInfo = document.createElement("div");
+        itemCardInfo.classList = "itemInfo"
+
+        let itemTitle = document.createElement("h2");
+        let itemText = document.createElement("p");
+        let itemImg = document.createElement("img");
+        itemImg.classList = "itemImage";
+        itemImg.setAttribute("src", "/assets/" + selectedItem.image); //eftersom bilderna ej ligger i root-mappen
+        let itemPrice = document.createElement("h4");
+
+        let itemBtn = document.createElement("button");
+        itemBtn.classList = "addItemBtn";
+        itemBtn.name = selectedItem.title;
+        itemBtn.addEventListener("click", () => {
+            addToCart(this.name);
+        })
+        
+
+        itemTitle.innerText = selectedItem.title;
+        itemText.innerText = selectedItem.description;
+        itemImg.innerText = selectedItem.image;
+        itemPrice.innerText = selectedItem.price + " :-";
+        itemBtn.innerHTML = "Lägg i varukorg";
+
+        itemCardInfo.appendChild(itemTitle);
+        itemCardInfo.appendChild(itemText);
+        itemCardInfo.appendChild(itemImg);
+        itemCardInfo.appendChild(itemPrice);
+        itemCardInfo.appendChild(itemBtn);
+
+        itemCard.appendChild(itemCardInfo);
+        mainContainer.appendChild(itemCard);
+    }
+}
+
+function addToCart(title) {
+    let itemToAdd = title;
+
+    for (let i = 0; i < listOfProducts.length; i++) {
+        if (itemToAdd == listOfProducts[i].title) {
+            inCart.push(listOfProducts[i]);
+            let jsonString = JSON.stringify(inCart);
+            localStorage.doList = jsonString;
+
+            counter();
+
+        }
+    }
+}
+
+function counter() {
+    document.getElementById("itemCounter").innerHTML = inCart.length;
 }
